6a72f714a29175ced164966a3c7d08f13560d4af
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-01 23:40:02 +0100

    Initialisation project

---
+++ dontcontaminate/src/App.js
@@ -0,0 +1,19 @@
+import logo from './BottomBackground.png';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Don't Contaminate, Please !
+                </p>
+            </header>
+            <body className="App-body">
+                <img src={logo} className="App-logo" alt="logo" />
+            </body>
+        </div>
+    );
+}
+
+export default App;
